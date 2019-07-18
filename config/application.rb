00938ea99c0df05ebba5444130ae96b7717dd7e9
bhqaq <brendan.heindl@gmail.com>
2019-07-18 01:36:38 -0300

    Example CRUB MaxiKiosco

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MaxiKiosco
+module Store
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get '/products', to: 'products#index'
+  get '/products/new', to: 'products#new'
+  post '/products', to: 'products#create'
+  get '/product/:id', to: 'products#show', as: 'product'
+  get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
+  patch '/product/:id', to: 'products#update'
+  put '/product/:id', to: 'products#update'
+  delete '/product/:id', to: 'products#delete'
   #resources :products
-  get '/products', to:'products#index'
-  #get '/', to:'products#index'
-  get '/products/new', to:'products#new'
-  #get '/', to:'products#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
